2c5c1ab85132d02906a0e2098ba13f3f7cad9541
Elliott Watson <ElliottWatson0@gmail.com>
2024-09-09 13:47:20 -0400

    starting to add photos in site

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
 import { PageHeader } from './layouts/PageHeader'
+import Lightbox from 'yet-another-react-lightbox'
+import { slides } from './data'
 
 export default function App() {
+  const [open, setOpen] = useState<boolean>(false)
+
   return (
     <div className='max-h-screen flex flex-col'>
       <PageHeader />
+
+      <button onClick={() => setOpen(true)}>Open Light Box</button>
+
+      <Lightbox open={open} slides={slides} />
     </div>
   )
 }
---
+++ src/data.ts
@@ -0,0 +1,142 @@
+export const slides = [
+  {
+    src: 'https://drscdn.500px.org/photo/1099557302/q%3D90_m%3D1024/v2?sig=4d576a41c2523838da7dfea640c3e2b2143973b7309ecde6fd1ce19acdb42ee5',
+    title: 'Just a Look',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557301/q%3D90_m%3D1024/v2?sig=670f5200e0f53ca370f431ac848fb37e910c6ac2cd5cb1f4fb3f99e3146d2c65',
+    title: 'Glance Back',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557300/q%3D90_m%3D1024/v2?sig=3e44b5c359e4844cc54c3fabe4e9503cbe733a940fccfc97b3903f5161d24385',
+    title: 'Down by the Waves',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557299/q%3D90_m%3D1024/v2?sig=f6166b3800ae4a4a0ba63bdff8b5316c45c4580d5106e8962664f81957e6bc81',
+    title: 'Railing',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557298/q%3D90_m%3D1024/v2?sig=e5cfb8f2ff8e0f6c1df3e7e72897654c86b03a0fdaee9a5edf1a40f5d0ac6113',
+    title: 'Sunday',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557297/q%3D90_m%3D1024/v2?sig=73211f2eb186041ea2ac6cc2ff43fc415b9196f0bc90efe00cc96c4e5d96ccbc',
+    title: 'Balcony View',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557296/q%3D90_m%3D1024/v2?sig=dd8402479d1e85867814b044d7cbe7bab5998b7fa794ecb684876419981b0a55',
+    title: 'Graceful',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557294/q%3D90_m%3D1024/v2?sig=be4c9d1dd9603d91d11f57f0a57cd934006f7c8c13c071726df493a5d417852f',
+    title: 'The proposal',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557293/q%3D90_m%3D1024/v2?sig=7406b180a6be3122c30dcf12856200e6c619e2e581cf1804e7a66454f81e8802',
+    title: 'Headshot',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557292/q%3D90_m%3D1024/v2?sig=4a7d358564e5501060f99c53cf809b57e69e006b594eab5b6b017962355f2b9a',
+    title: 'Editorial',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557291/q%3D90_m%3D1024/v2?sig=2ba290b014b4ab1f00813210208c6e1fc4e038016b908c7c9f62dabcd40a24ab',
+    title: 'Immense',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557289/q%3D90_m%3D1024/v2?sig=cf9e39206f90504ce3e424f40b816b5be13b2ff2cc13892240dddf720d2b98ac',
+    title: 'FEEL',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557287/q%3D90_m%3D1024/v2?sig=ca6b07a317bb1dafa6e34ff752abc02baf6c03cd2ffadfab1d71e5d0d6d59d13',
+    title: 'Stun',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557286/q%3D90_m%3D1024/v2?sig=4006fa1be473912246f5b20b44a0275176d2bc9afe9089dc97aa244307b906e2',
+    title: 'Cover Art',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557285/q%3D90_m%3D1024/v2?sig=f0df8e083e537a399c7e092c8bfa34665a48c8724a610d03ebd90618c5eb2c78',
+    title: 'Digitals',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557284/q%3D80_m%3D600/v2?sig=bb08c6fe8d993fe378236637fdf2b43df57bd0b9bf0e9cd714ad1d87c727cb3b',
+    title: 'Kiss',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557282/q%3D90_m%3D1024/v2?sig=c12dd449cbc62ebbdbdb5f932ca7ed4e4a6e89b98a8d42998db65800bee33890',
+    title: 'Boneless',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557280/q%3D90_m%3D1024/v2?sig=bbb1a0e3442eca6ed1e7a084a33c71d5a80a2a3f834df127b3141b84a2c8ba4a',
+    title: 'The Hold',
+    description: '',
+  },
+  {
+    src: 'https://drscdn.500px.org/photo/1099557279/q%3D90_m%3D1024/v2?sig=a19e9f0549c556b3a9d4ecfdd8622cde2248e8deb976139937cf64d4105f70ce',
+    title: 'Realtor',
+    description: '',
+  },
+  {
+    src: '',
+    title: '',
+    description: '',
+  },
+  {
+    src: '',
+    title: '',
+    description: '',
+  },
+  {
+    src: '',
+    title: '',
+    description: '',
+  },
+  {
+    src: '',
+    title: '',
+    description: '',
+  },
+  {
+    src: '',
+    title: '',
+    description: '',
+  },
+  {
+    src: '',
+    title: '',
+    description: '',
+  },
+  {
+    src: '',
+    title: '',
+    description: '',
+  },
+  {
+    src: '',
+    title: '',
+    description: '',
+  },
+  {
+    src: '',
+    title: '',
+    description: '',
+  },
+]
